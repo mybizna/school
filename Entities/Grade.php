50afe2471abe620ab5cdf5513a5af49531507f91
Dedan Irungu <dedanirungu@gmail.com>
2023-07-26 14:00:15 +0300

    adding listtable and formbuilder fuction to all model so that data can be rendered via json

--- Entities/Grade.php
+++ Entities/Grade.php
@@ -2,8 +2,10 @@
 
 namespace Modules\School\Entities;
 
-use Modules\Base\Entities\BaseModel;
 use Illuminate\Database\Schema\Blueprint;
+use Modules\Base\Entities\BaseModel;
+use Modules\Core\Classes\Views\FormBuilder;
+use Modules\Core\Classes\Views\ListTable;
 
 class Grade extends BaseModel
 {
@@ -12,6 +14,38 @@ class Grade extends BaseModel
     public $migrationDependancy = [];
     protected $table = "school_grade";
 
+    public function listTable()
+    {
+        // listing view fields
+        $fields = new ListTable();
+
+        $fields->name('name')->type('text')->ordering(true);
+
+        return $fields;
+
+    }
+
+    public function formBuilder()
+    {
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('name')->type('text')->group('w-1/2');
+
+        return $fields;
+
+    }
+
+    public function filter()
+    {
+        // listing view fields
+        $fields = new FormBuilder();
+
+        $fields->name('name')->type('text')->group('w-1/6');
+
+        return $fields;
+
+    }
     /**
      * List of fields for managing postings.
      *
