2901cbf9008eb2270a81817ae8c42157d01e785b
Dedan Irungu <dedanirungu@gmail.com>
2023-08-03 18:03:55 +0300

    Changes to files: Entities/ClassYear.php Entities/Grade.php Entities/School.php Entities/Subject.php

--- Entities/School.php
+++ Entities/School.php
@@ -3,31 +3,39 @@
 namespace Modules\School\Entities;
 
 use Illuminate\Database\Schema\Blueprint;
-use Modules\Base\Entities\BaseModel;
 use Modules\Base\Classes\Views\FormBuilder;
 use Modules\Base\Classes\Views\ListTable;
+use Modules\Base\Entities\BaseModel;
 
 class School extends BaseModel
 {
     /**
      * The fields that can be filled
+     *
      * @var array<string>
      */
     protected $fillable = ['name'];
 
     /**
      * List of tables names that are need in this model during migration.
+     *
      * @var array<string>
      */
     public array $migrationDependancy = [];
 
     /**
      * The table associated with the model.
+     *
      * @var string
      */
     protected $table = "school";
 
-    public function  listTable(): ListTable
+    /**
+     * Function for defining list of fields in table view.
+     *
+     * @return ListTable
+     */
+    public function listTable(): ListTable
     {
         // listing view fields
         $fields = new ListTable();
@@ -38,7 +46,14 @@ class School extends BaseModel
 
     }
 
-    public function formBuilder()
+/**
+ * Function for defining list of fields in form view.
+ *
+ * @var string
+ *
+ * @return FormBuilder
+ */
+    public function formBuilder(): FormBuilder
     {
         // listing view fields
         $fields = new FormBuilder();
@@ -48,7 +63,13 @@ class School extends BaseModel
         return $fields;
 
     }
-
+/**
+ * Function for defining list of fields in form view.
+ *
+ * @var string
+ *
+ * @return FormBuilder
+ */
     public function filter(): FormBuilder
     {
         // listing view fields
@@ -65,7 +86,7 @@ class School extends BaseModel
      * @param Blueprint $table
      * @return void
      */
-    public function migration(Blueprint $table)
+    public function migration(Blueprint $table): void
     {
         $table->increments('id');
         $table->string('name');
--- Entities/Subject.php
+++ Entities/Subject.php
@@ -11,22 +11,30 @@ class Subject extends BaseModel
 {
     /**
      * The fields that can be filled
+     *
      * @var array<string>
      */
     protected $fillable = ['name'];
 
     /**
      * List of tables names that are need in this model during migration.
+     *
      * @var array<string>
      */
     public array $migrationDependancy = [];
 
     /**
      * The table associated with the model.
+     *
      * @var string
      */
     protected $table = "school_subject";
 
+    /**
+     * Function for defining list of fields in table view.
+     *
+     * @return ListTable
+     */
     public function listTable(): ListTable
     {
         // listing view fields
@@ -38,7 +46,12 @@ class Subject extends BaseModel
 
     }
 
-    public function formBuilder()
+    /**
+     * Function for defining list of fields in form view.
+     * 
+     * @return FormBuilder
+     */
+    public function formBuilder(): FormBuilder
     {
         // listing view fields
         $fields = new FormBuilder();
@@ -49,6 +62,11 @@ class Subject extends BaseModel
 
     }
 
+    /**
+     * Function for defining list of fields in form view.
+     * 
+     * @return FormBuilder
+     */
     public function filter(): FormBuilder
     {
         // listing view fields
@@ -65,7 +83,7 @@ class Subject extends BaseModel
      * @param Blueprint $table
      * @return void
      */
-    public function migration(Blueprint $table)
+    public function migration(Blueprint $table): void
     {
         $table->increments('id');
         $table->string('name');
