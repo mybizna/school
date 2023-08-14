d2ad887dd50858b7ca2abdd3570e4b52f52cda38
Dedan Irungu <dedanirungu@gmail.com>
2023-08-14 21:45:54 +0300

    Changes to files: Entities/ClassYear.php Entities/Grade.php Entities/School.php Entities/Subject.php

--- Entities/ClassYear.php
+++ Entities/ClassYear.php
@@ -3,8 +3,6 @@
 namespace Modules\School\Entities;
 
 use Illuminate\Database\Schema\Blueprint;
-use Modules\Base\Classes\Views\FormBuilder;
-use Modules\Base\Classes\Views\ListTable;
 use Modules\Base\Entities\BaseModel;
 
 class ClassYear extends BaseModel
@@ -38,62 +36,15 @@ class ClassYear extends BaseModel
     protected $table = "school_class_year";
 
     /**
-     * Function for defining list of fields in table view.
-     *
-     * @return ListTable
-     */
-    public function listTable(): ListTable
-    {
-        // listing view fields
-        $fields = new ListTable();
-
-        $fields->name('name')->html('text')->ordering(true);
-
-        return $fields;
-
-    }
-
-    /**
-     * Function for defining list of fields in form view.
-     *
-     * @return FormBuilder
-     */
-    public function formBuilder(): FormBuilder
-    {
-        // listing view fields
-        $fields = new FormBuilder();
-
-        $fields->name('name')->html('text')->group('w-1/2');
-
-        return $fields;
-
-    }
-
-    /**
-     * Function for defining list of fields in filter view.
-     *
-     * @return FormBuilder
-     */
-    public function filter(): FormBuilder
-    {
-        // listing view fields
-        $fields = new FormBuilder();
-
-        $fields->name('name')->html('text')->group('w-1/6');
-
-        return $fields;
-
-    }
-    /**
      * List of fields to be migrated to the datebase when creating or updating model during migration.
      *
      * @param Blueprint $table
      * @return void
      */
-    public function migration(Blueprint $table): void
+    public function fields(Blueprint $table): void
     {
-        $this->fields->increments('id');
-        $this->fields->string('name');
-        $this->fields->string('description');
+        $this->fields->increments('id')->html('text');
+        $this->fields->string('name')->html('text');
+        $this->fields->string('description')->html('textarea');
     }
 }
