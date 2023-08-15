7c0ad2a3f2c11688f2bce4a5916db198c0e69e92
Dedan Irungu <dedanirungu@gmail.com>
2023-08-15 10:24:20 +0300

    Changes to files: Entities/ClassYear.php Entities/Grade.php Entities/School.php Entities/Subject.php

--- Entities/School.php
+++ Entities/School.php
@@ -43,6 +43,8 @@ class School extends BaseModel
      */
     public function fields(Blueprint $table): void
     {
+        $this->fields = $table ?? new Blueprint($this->table);
+        
         $this->fields->increments('id')->html('text');
         $this->fields->string('name')->html('text');
         $this->fields->string('description')->html('textarea');
