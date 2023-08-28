4efb7b3433cc5bf43113ce27126221b5eaf4e326
Dedan Irungu <dedanirungu@gmail.com>
2023-08-28 13:24:13 +0300

    changing field id from text to hidden

--- Entities/School.php
+++ Entities/School.php
@@ -45,7 +45,7 @@ class School extends BaseModel
     {
         $this->fields = $table ?? new Blueprint($this->table);
 
-        $this->fields->increments('id')->html('text');
+        $this->fields->increments('id')->html('hidden');
         $this->fields->string('name')->html('text');
         $this->fields->string('description')->html('textarea');
         $this->fields->string('published')->html('switch');
