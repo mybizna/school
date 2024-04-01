bb395f1205a9f18d3543d637f56949bf3e1899ce
Dedan <dedanirungu@gmail.com>
2024-04-01 19:30:26 +0300

    Add rights for school, class, grade, and subject

--- right.php
+++ right.php
@@ -8,3 +8,26 @@ $this->add_right("school", "school", "supervisor", view:true, add:true, edit:tru
 $this->add_right("school", "school", "staff", view:true, add:true, edit:true);
 $this->add_right("school", "school", "registered", view:true, add:true);
 $this->add_right("school", "school", "guest", view:true, );
+
+$this->add_right("school", "class", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("school", "class", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("school", "class", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("school", "class", "staff", view:true, add:true, edit:true);
+$this->add_right("school", "class", "registered", view:true, add:true);
+$this->add_right("school", "class", "guest", view:true, );
+
+
+$this->add_right("school", "grade", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("school", "grade", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("school", "grade", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("school", "grade", "staff", view:true, add:true, edit:true);
+$this->add_right("school", "grade", "registered", view:true, add:true);
+$this->add_right("school", "grade", "guest", view:true, );
+
+$this->add_right("school", "subject", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("school", "subject", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("school", "subject", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("school", "subject", "staff", view:true, add:true, edit:true);
+$this->add_right("school", "subject", "registered", view:true, add:true);
+$this->add_right("school", "subject", "guest", view:true, );
+
