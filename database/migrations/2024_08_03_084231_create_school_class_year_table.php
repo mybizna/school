a1f07595d9dcce0598eb6b4a89910b9967890fa0
Dedan <dedanirungu@gmail.com>
2024-09-27 10:12:54 +0300

    Changes to files: database/migrations/2024_08_03_084231_create_school_class_year_table.php database/migrations/2024_08_03_084239_create_school_grade_table.php database/migrations/2024_08_03_084247_create_school_school_table.php database/migrations/2024_08_03_084301_create_school_subject_table.php database/migrations/2024_09_03_135116_add_deleted_at_to_school_class_year_table.php database/migrations/2024_09_03_135123_add_deleted_at_to_school_grade_table.php database/migrations/2024_09_03_135131_add_deleted_at_to_school_school_table.php database/migrations/2024_09_03_135142_add_deleted_at_to_school_subject_table.php

--- database/migrations/2024_08_03_084231_create_school_class_year_table.php
+++ database/migrations/2024_08_03_084231_create_school_class_year_table.php
@@ -18,7 +18,12 @@ return new class extends Migration
             $table->string('description');
             $table->string('published');
 
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->onDelete('set null');
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2024_08_03_084301_create_school_subject_table.php
+++ database/migrations/2024_08_03_084301_create_school_subject_table.php
@@ -17,8 +17,13 @@ return new class extends Migration
             $table->string('name');
             $table->string('description');
             $table->string('published');
-            
+
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->foreignId('deleted_by')->nullable()->constrained('users')->onDelete('set null');
+
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
